a5070d772e08757ca792cf9e6014bb3810ff9bef
StefanyFerreiraFlores <stefanyferreiraflores82@gmail.com>
2025-02-25 10:18:41 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Configurações de acesso ao banco de dados
+
+    define('DB_HOST','localhost');
+    define('DB_USER','root');
+    define('DB_PASSWORD','');
+    define('DB_NAME','petlove');
+
+?>
